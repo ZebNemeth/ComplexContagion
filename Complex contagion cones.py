90689e77203b613705b489c68f4c0aec57634014
aartvanbochove <64839449+aartvanbochove@users.noreply.github.com>
2020-05-20 17:48:42 +0200

    Update Complex contagion cones.py
    - Naming is made slightly easier
    - I hope I explain myself better. However I have the feeling I might have made it worse
    - Deleted the random part to make it reproducable

--- Complex contagion cones.py
+++ Complex contagion cones.py
@@ -1,37 +1,52 @@
 import networkx as nx
-import random
 import matplotlib.pyplot as plt
 import math
 import numpy as np
 
-#First setting up the variables for the model
+'Defining the function for the name labeling.'
+def labelNode(conenr, levelnr, nodenr):
+    return(str(str(conenr) + "_" + str(levelnr) + "_" + str(nodenr)))
+    
+'Setting up the variables for the model'
 nCones = 8 #The number of cones (communities)
 nLevels = 4 #The number of levels per cone
-nNodes = np.array(4) #The number of nodes per level. For now only the highest level
-for i in range(1,nLevels): nNodes = np.append(nNodes,np.max(nNodes)*8) #Number of nodes set for other levels
-nUpperNeighbors = np.arange(nLevels) #Number of upper neighbors per node in the ring. Different per level
-nRingNeighbors = (nUpperNeighbors+1)*2 #Number of neighbors in the ring. Different per level
 
-#Graph is made
+#The number of nodes per level. Example: with 3 levels this creates [4,32,256]
+nNodes = np.array(4)
+for i in range(1,nLevels): nNodes = np.append(nNodes,np.max(nNodes)*8)
+
+nRingNeighbors = (np.arange(nLevels)+1)*2 #Number of neighbors per node in the ring. Example with 3 levels: [2,4,6]
+
+
+'Graph is defined'
 G = nx.Graph()
 
-#Setting up the cones
-for level in range(nLevels): #Building it level by level, from the highest level to the lowest
-    ringlevel = nx.watts_strogatz_graph(nNodes[level],nRingNeighbors[level],0) #Making the Watts-Strogatz ring for this level
-    for cone in range(nCones): #Building this ring for every cone
+
+'Cones are created'
+for level in reversed(range(nLevels)): #Building it level by level, from the lowest level to the highest
+
+    'The seperate rings are build'
+    #Making the 'blueprint' of the Watts-Strogatz ring for this level:
+    ringlevel = nx.watts_strogatz_graph(nNodes[level],nRingNeighbors[level],0)
+    #Implementing this blueprint for every cone
+    for cone in range(nCones):
         mapping = {}
         for node in range(nNodes[level]):
-            mapping.update({node:str(str(cone) + "_" + str(level) + "_" + str(node))}) #Making a map for relabeling the nodes as conenr_levelnr_nodenr
-        ring = nx.relabel_nodes(ringlevel,mapping) #Applying map on ring for this cone
+            mapping.update({node:labelNode(cone,level,node)}) #Making a map for relabeling the node names as conenr_levelnr_nodenr
+        ring = nx.relabel_nodes(ringlevel,mapping) #Applying map on ring for this cone - this relabels the node names
+
         G.add_nodes_from(ring) #Adding the nodes from the ring to the graph
         G.add_edges_from(ring.edges()) #Adding the edges from the ring to the graph
-        if level != 0: #For making connections with level above
-            nGroups = nNodes[level-1] #Nodes are split into groups - this is the number of groups
-            groupsize = nNodes[level]/nGroups #Number of nodes per group
-            connectedNodes = [] #Array which will be filled with nodes to connect to
-            for group in range(nGroups):
-                connectedNodes.append(random.sample(range(0,nNodes[level-1]),nUpperNeighbors[level])) #For every group, nUpperNeighbors[level] nodes will be randomly selected from te level above.
-            for node in range(nNodes[level]):
-                group = math.ceil((node+1)/groupsize)-1 #The group this node belongs to
-                for upperNeighbor in range(0,nUpperNeighbors[level]):
-                    G.add_edge(str(str(cone) + "_" + str(level-1) + "_" + str(connectedNodes[group][upperNeighbor])),str(str(cone) + "_" + str(level) + "_" + str(node))) #Edges are added between the different levels
+        
+        'The connections between rings are created. The connections are created from the higher level to the lower level'
+        if level != nLevels-1: #The lowest level can't create any connections up, so this level is skipped
+            #The nodes of the level below are split into groups. This is the size of each group
+            groupsize = nNodes[level+1]/nNodes[level]
+            
+            for nodeInLowerLevel in range(nNodes[level+1]): #This loop goes over every node in the lower level
+                nodeInThisLevel = math.ceil((nodeInLowerLevel+1)/groupsize)-1 #This is the nodenumber of the node in the current level.
+                G.add_edge(labelNode(cone,level,nodeInThisLevel),labelNode(cone,level+1,nodeInLowerLevel)) #Edges are added between the different levels
+                
+'Graph is drawn'
+#nx.draw(G, node_size=10, edge_color='grey')
+#plt.show()
